c6ed46fe01c1715f0eb5018992b27e2b8f22feea
innoflash <innocentmazando@gmail.com>
2020-06-15 14:29:43 +0200

    added pagination links and meta

---
+++ src/lib/responses/paginated.response.ts
@@ -0,0 +1,5 @@
+import {CollectionResponse} from './collection.response';
+
+export interface PaginatedResponse<T> extends CollectionResponse<T>{
+
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -6,3 +6,7 @@ export * from './lib/laravel-utils.service';
 export * from './lib/laravel-utils.component';
 export * from './lib/laravel-utils.module';
 export * from './lib/responses/action.response';
+export * from './lib/responses/collection.response';
+export * from './lib/responses/paginated.response.js';
+export * from './lib/responses/pagination/links';
+export * from './lib/responses/pagination/meta';
